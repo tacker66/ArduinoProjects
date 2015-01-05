3825f53872202ebfeda32d606e7a8caef6965d9a
Thomas Ackermann <th.acker@arcor.de>
2015-01-05 13:34:32 +0100

    Testing Arduino's low power capabilities

---
+++ LowPower/LowPower.ino
@@ -0,0 +1,24 @@
+
+#include <Arduino.h>
+#include <LowPower.h>
+
+int builtin_led = 13;
+int aux_led = 8;
+
+void setup()
+{
+	pinMode(builtin_led, OUTPUT);
+	pinMode(aux_led, OUTPUT);
+}
+
+void loop()
+{
+	digitalWrite(aux_led, HIGH);
+	delay(8000);
+	digitalWrite(aux_led, LOW);
+	delay(8000);
+	digitalWrite(builtin_led, HIGH);
+	delay(8000);
+	digitalWrite(builtin_led, LOW);
+	LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+}
